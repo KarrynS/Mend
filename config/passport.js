678a8917eb0cae9eb2a5ef72b1c1f5320963d893
KarrynS <karrynsaw@gmail.com>
2021-04-22 22:33:28 +1000

    updating package json

--- config/passport.js
+++ config/passport.js
@@ -1,40 +1,40 @@
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
+// const passport = require("passport");
+// const LocalStrategy = require("passport-local").Strategy;
 
-const User = require("../models/user");
+// const User = require("../models/user");
 
-// Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.getUserByUsername(username, function(err, user){
-      if(err) throw err;
-      if(!user){
-        return done(null, false, {message: 'Unknown User'});
-      }
-      User.comparePassword(password, user.password, function(err, isMatch){
-        if(err) throw err;
-     	if(isMatch){
-     	  return done(null, user);
-     	} else {
-     	  return done(null, false, {message: 'Invalid password'});
-     	}
-     });
-   });
-  }
-));
+// // Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
+// passport.use(new LocalStrategy(
+//   function(username, password, done) {
+//     User.getUserByUsername(username, function(err, user){
+//       if(err) throw err;
+//       if(!user){
+//         return done(null, false, {message: 'Unknown User'});
+//       }
+//       User.comparePassword(password, user.password, function(err, isMatch){
+//         if(err) throw err;
+//      	if(isMatch){
+//      	  return done(null, user);
+//      	} else {
+//      	  return done(null, false, {message: 'Invalid password'});
+//      	}
+//      });
+//    });
+//   }
+// ));
 
-// In order to help keep authentication state across HTTP requests,
-// Sequelize needs to serialize and deserialize the user
-// Just consider this part boilerplate needed to make it all work
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
+// // In order to help keep authentication state across HTTP requests,
+// // Sequelize needs to serialize and deserialize the user
+// // Just consider this part boilerplate needed to make it all work
+// passport.serializeUser(function(user, done) {
+//   done(null, user.id);
+// });
 
-passport.deserializeUser(function(id, done) {
-  User.getUserById(id, function(err, user) {
-    done(err, user);
-  });
-});
+// passport.deserializeUser(function(id, done) {
+//   User.getUserById(id, function(err, user) {
+//     done(err, user);
+//   });
+// });
 
-// Exporting our configured passport
-module.exports = passport;
+// // Exporting our configured passport
+// module.exports = passport;
