67c03a5d5437f01133a403fc2a9edae263a8bd85
KarrynS <karrynsaw@gmail.com>
2021-05-01 23:59:55 +1000

    deploying

---
+++ client/build/precache-manifest.16252cf6597aa784a67b3af5ea9a034f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1141dc8fc3a3c6adab7288ead6e6e2d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b0617e6655f3fabaf0f",
+    "url": "/static/css/2.02adba32.chunk.css"
+  },
+  {
+    "revision": "a5e1fa9315ac465fee9a",
+    "url": "/static/css/main.4e264e8c.chunk.css"
+  },
+  {
+    "revision": "6b0617e6655f3fabaf0f",
+    "url": "/static/js/2.d782399c.chunk.js"
+  },
+  {
+    "revision": "47af5642237abca2556609f1b6e53e3f",
+    "url": "/static/js/2.d782399c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5e1fa9315ac465fee9a",
+    "url": "/static/js/main.9f7496b8.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "e90a0d8a11271a225950fdca399d39ac",
+    "url": "/static/media/dark-green-texture.e90a0d8a.jpg"
+  },
+  {
+    "revision": "d46c86317b0d9457a55f4644032427f3",
+    "url": "/static/media/diagnosis_book.d46c8631.jpg"
+  },
+  {
+    "revision": "e121a02bf7ea573ff9e939b178228478",
+    "url": "/static/media/eyeissue.e121a02b.jpg"
+  },
+  {
+    "revision": "9169418f164f8f3f4cec815384775a3e",
+    "url": "/static/media/glassesonbook.9169418f.jpg"
+  },
+  {
+    "revision": "eb27cd0884ac9d8ea6390722ff7ffeac",
+    "url": "/static/media/leaf.eb27cd08.jpg"
+  },
+  {
+    "revision": "c92274f95b0ba86aabf56caac486e2b1",
+    "url": "/static/media/modeleye.c92274f9.jpg"
+  },
+  {
+    "revision": "e9a47341f56156e2fdbc9beb054400c4",
+    "url": "/static/media/optomlogin.e9a47341.jpg"
+  },
+  {
+    "revision": "c64835c7a14e2d96f464f9780ae5bc32",
+    "url": "/static/media/optomsignup.c64835c7.jpg"
+  },
+  {
+    "revision": "8e550e04df98e6693c950e782acb1bb4",
+    "url": "/static/media/rainboweye.8e550e04.jpg"
+  }
+]);
