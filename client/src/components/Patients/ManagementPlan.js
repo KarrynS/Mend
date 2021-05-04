aa5e75bfea1ab7f241d73ddd82eed1f6bf81c7e6
KarrynS <karrynsaw@gmail.com>
2021-05-04 21:38:20 +1000

    clearing management plan form

--- client/src/components/Patients/ManagementPlan.js
+++ client/src/components/Patients/ManagementPlan.js
@@ -46,7 +46,7 @@ const ManagementPlan = (props) => {
        console.log("New Management added", formObject)
        console.log("patientId" ,patientId)
        alert("Treatment plan recorded")
-
+       
     }
     return (
         <>
@@ -56,19 +56,19 @@ const ManagementPlan = (props) => {
                 <div class="form-group row">
                     <label htmlFor="Diagnosis" class="col-sm-2 col-form-label">Diagnosis:</label>
                     <div class="col-sm-10">
-                    <input onChange={handleInputChange} name="diagnosis" type="text" class="form-control" id="inputdiagnosis" placeholder="Enter Diagnosis"/>
+                    <input onChange={handleInputChange} value={formObject.diagnosis} name="diagnosis" type="text" class="form-control" id="inputdiagnosis" placeholder="Enter Diagnosis"/>
                     </div>
                 </div>
                 <div class="form-group row">
                     <label htmlFor="Management" class="col-sm-2 col-form-label">Treatment:</label>
                     <div class="col-sm-10">
-                    <textarea onChange={handleInputChange} name="treatment" type="text" row="3" class="form-control" id="inputmanagement" placeholder="Enter Management Plan"></textarea>
+                    <textarea onChange={handleInputChange} value={formObject.treatment} name="treatment" type="text" row="3" class="form-control" id="inputmanagement" placeholder="Enter Management Plan"></textarea>
                     </div>
                 </div>
                 <div class="form-group row">
                     <label htmlFor="example-date-input" class="col-2 col-form-label">Review Date:</label>
                     <div class="col-sm-10">
-                    <input onChange={handleInputChange} name="review" type="text" class="form-control" id="inputreview" placeholder="Suggested review schedule"/>
+                    <input onChange={handleInputChange} value={formObject.review}name="review" type="text" class="form-control" id="inputreview" placeholder="Suggested review schedule"/>
                     </div>
                 </div>
                 <div class="form-group row">
