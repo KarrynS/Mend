72b793538f4f3046f2d1ec2a57917bf4689d0833
KarrynS <karrynsaw@gmail.com>
2021-05-10 07:39:20 +1000

    Heroku changes

--- server.js
+++ server.js
@@ -23,6 +23,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+
+
+
+
 // We need to use sessions to keep track of our user's login status
 app.use(session(
   { secret: "keyboard cat", 
