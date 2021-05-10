dd399d5542e80b8d0016a10e08342ba05da9646f
KarrynS <karrynsaw@gmail.com>
2021-05-10 13:21:02 +1000

    insalling express-ws

--- client/src/components/SocketChat/InitialChat.js
+++ client/src/components/SocketChat/InitialChat.js
@@ -4,12 +4,14 @@ import "./style.css";
 import TopbarOptom from "../Topbar/TopbarOptom";
 import Navbar from "../Navbar/Navbar";
 
-let socket;
+
 // let socket = io('ws://mend2021.herokuapp.com/socket.io/?EIO=4&transport=websocket');
 // to deploy
+let socket;
 const CONNECTION_PORT = 'ws://mend2021.herokuapp.com/socket.io/?EIO=4&transport=websocket'
 
 // connecting locally
+// let socket;
 // const CONNECTION_PORT = 'localhost:3000/'
 
 
